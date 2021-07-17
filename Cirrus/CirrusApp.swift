5c3e431ad7c02e7f8b1459f5738033872740f1e9
Ben Gottlieb <ben@standalone.com>
2021-07-16 19:07:45 -0500

    Initial Commit

---
+++ Cirrus/CirrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CirrusApp.swift
+//  Cirrus
+//
+//  Created by Ben Gottlieb on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CirrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
