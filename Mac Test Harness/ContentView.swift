983a101264045b3c38d0ad9b62f22a4a1f5cbcae
Ben Gottlieb <ben@standalone.com>
2021-09-06 19:22:45 -0500

    updated mac content view

--- Mac Test Harness/ContentView.swift
+++ Mac Test Harness/ContentView.swift
@@ -32,6 +32,11 @@ struct ContentView: View {
 						.font(.caption)
 						.padding()
 						.multilineTextAlignment(.center)
+				case .temporaryUnavailable:
+					Text("Temporarily Unavailable")
+						.font(.caption)
+						.padding()
+						.multilineTextAlignment(.center)
 				}
 				
 			}
