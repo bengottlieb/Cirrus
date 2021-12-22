eeac96c7aa1a0f0894b6add55103bcd78f9c731e
Ben Gottlieb <ben@standalone.com>
2021-12-22 09:40:23 -0500

    fixed deprecated Task.sleep call

--- Source/Cirrus/Types/AsyncRecordSequence.swift
+++ Source/Cirrus/Types/AsyncRecordSequence.swift
@@ -89,7 +89,7 @@ public class AsyncRecordSequence: AsyncSequence {
 				}
 				
 				if sequence.isComplete { return nil }
-				await Task.sleep(1_000)
+				try? await Task.sleep(nanoseconds: 1_000)
 			}
 		}
 		
