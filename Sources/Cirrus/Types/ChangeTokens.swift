31834c0d9638569735802b4564e855bd5983fc1c
Ben Gottlieb <ben@standalone.com>
2023-06-23 20:50:36 -0500

    improved changeToken storage

--- Sources/Cirrus/Types/ChangeTokens.swift
+++ Sources/Cirrus/Types/ChangeTokens.swift
@@ -10,30 +10,49 @@ import CloudKit
 
 public class ChangeTokens: Codable {
 	public var tokens: [String: Data] = [:]
+	var url: URL?
 	
 	public init(saved: [String: Data]? = nil) {
 		tokens = saved ?? [:]
 	}
 	
-	func changeToken(for zoneID: CKRecordZone.ID) -> CKServerChangeToken? {
+	public static func tokens(at url: URL) -> ChangeTokens {
+		let tokens = (try? ChangeTokens.loadJSON(file: url)) ?? ChangeTokens()
+		tokens.url = url
+		return tokens
+	}
+	
+	func save() {
+		if let url {
+			try? self.saveJSON(to: url)
+		}
+	}
+	
+	public func changeToken(for zoneID: CKRecordZone.ID) -> CKServerChangeToken? {
 		guard let data = tokens[zoneID.tokenIdentifier] else { return nil }
 		return try? NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data)
 	}
 	
-	func changeToken(for database: CKDatabase) -> CKServerChangeToken? {
+	public func changeToken(for database: CKDatabase) -> CKServerChangeToken? {
 		guard let data = tokens[database.databaseScope.tokenIdentifier] else { return nil }
 		return try? NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data)
 	}
 	
-	func setChangeToken(_ token: CKServerChangeToken, for zoneID: CKRecordZone.ID) {
+	public func setChangeToken(_ token: CKServerChangeToken, for zoneID: CKRecordZone.ID) {
 		tokens[zoneID.tokenIdentifier] = token.data
+		save()
 	}
 	
-	func setChangeToken(_ token: CKServerChangeToken, for database: CKDatabase) {
+	public func setChangeToken(_ token: CKServerChangeToken, for database: CKDatabase) {
 		tokens[database.databaseScope.tokenIdentifier] = token.data
+		save()
 	}
 	
-	func clear() { tokens = [:] }
+	public func clear() {
+		tokens = [:]
+		save()
+	}
+	
 	public var isEmpty: Bool { tokens.isEmpty }
 	
 	func tokens(for zoneIDs: [CKRecordZone.ID]) -> [CKRecordZone.ID : CKFetchRecordZoneChangesOperation.ZoneConfiguration] {
