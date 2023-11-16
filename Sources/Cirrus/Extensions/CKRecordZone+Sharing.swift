21a456f2a0f7c1b1bcde372875f1646333674396
Ben Gottlieb <ben@standalone.com>
2023-11-15 18:17:13 -0600

    try to fetch a share before creating it

--- Sources/Cirrus/Extensions/CKRecordZone+Sharing.swift
+++ Sources/Cirrus/Extensions/CKRecordZone+Sharing.swift
@@ -10,6 +10,11 @@ import SwiftUI
 
 extension CKDatabase {
 	public func share(zoneID: CKRecordZone.ID, readWrite: Bool = false) async throws -> CKShare {
+        let recordID = CKRecord.ID(recordName: "cloudkit.zoneshare", zoneID: zoneID)
+        
+        if let record = try? await fetchRecord(withID: recordID) as? CKSharex {
+            return record
+        }
 		let share = CKShare(recordZoneID: zoneID)
 		share.publicPermission = readWrite ? .readWrite : .readOnly
 		let result = try await save(share)
